a74cc19fce2f6e52f5e0c2074c83b67e220f1c5b
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-06-27 01:35:01 +0000

    feat: add client side rendering to brackets page

--- src/app/brackets/page.tsx
+++ src/app/brackets/page.tsx
@@ -1,23 +1,41 @@
-import postgres from 'postgres'
+'use client'
+
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { MoonLoader } from 'react-spinners'
 
 import { Bracket } from '@/utils/types'
 
 import Tables from './_tables'
 import styles from './styles.module.scss'
 
-async function getBrackets(): Promise<Bracket[]> {
-  const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' })
-  const result = await sql<Bracket[]>`SELECT * FROM simples_nacional`
+export default function Page() {
+  const [brackets, setBrackets] = useState<Bracket[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const { theme } = useTheme()
 
-  return result
-}
+  useEffect(() => {
+    init()
+  }, [])
 
-export default async function Page() {
-  const brackets = await getBrackets()
+  async function init() {
+    const response = await fetch('/api/brackets', { cache: 'no-store' })
+
+    const brackets = (await response.json()) as Bracket[]
+
+    setBrackets(brackets)
+    setIsLoading(false)
+  }
 
   return (
     <main className={styles.root}>
-      <Tables brackets={brackets} />
+      {isLoading ? (
+        <div className={styles.spinner}>
+          <MoonLoader speedMultiplier={0.7} size={100} color={theme === 'dark' ? 'white' : 'black'} />
+        </div>
+      ) : (
+        <Tables brackets={brackets} />
+      )}
     </main>
   )
 }
