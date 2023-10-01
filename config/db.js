3a9f1382a43fa7823697db46f1d405d5138ebe43
Matheus Eckel Kleinschmidt <matheus.kleinschmidt@catolicasc.edu.br>
2023-10-01 19:50:02 -0300

    Corrigido instancia para pegar variáveis do ambiente

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
-  dialect: 'postgres', // Especifica o dialeto como PostgreSQL
-  database: 'nome_do_banco_de_dados', // Substitua pelo nome do seu banco de dados
-  username: 'seu_usuario', // Substitua pelo seu nome de usuário
-  password: 'sua_senha', // Substitua pela sua senha
-  host: 'localhost', // Substitua pelo host do seu banco de dados
-  port: 5432, // Porta padrão do PostgreSQL
+  dialect: 'postgres', 
+  database: process.env.DB_NAME, 
+  username: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
+  host: process.env.DB_HOST, 
+  port: 5432, 
 });
 
 sequelize
