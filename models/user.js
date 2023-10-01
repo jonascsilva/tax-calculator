972bd88eb44878a684c427a0ca55fe5cbbf83e66
Matheus Eckel Kleinschmidt <matheus.kleinschmidt@catolicasc.edu.br>
2023-10-01 18:52:47 -0300

    Adicionando Model

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('<sua instância do Sequelize>');
+
+class SimplesNacional extends Model {}
+
+SimplesNacional.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
+    rbt12: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    nominalrate: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    deduction: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    irpj: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    csll: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cofins: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    pis: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cpp: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    icms: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'SimplesNacional',
+    tableName: 'simples_nacional',
+    timestamps: false,
+  }
+);
+
+module.exports = SimplesNacional;
