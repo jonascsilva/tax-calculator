60cb7fbb91306330177c7f0d830deee46f8dbf5c
Matheus Eckel Kleinschmidt <matheus.kleinschmidt@catolicasc.edu.br>
2023-10-01 18:52:08 -0300

    Adicionando migration

---
+++ migrations/20230929233032-create-simples-nacional.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('simples_nacional', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      rbt12: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      nominalrate: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      deduction: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      irpj: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      csll: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cofins: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      pis: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cpp: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      icms: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('simples_nacional');
+  },
+};
